b9473b00699a457a9f9ec83f3f9dd0166ea4e824
davidgarcial <d.alex10092@gmail.com>
2021-07-08 08:11:42 -0500

    Apply theme context in hooks

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { ThemeContext } from './App';
 
 export default function CounterHooks({ initialCount }) {
     console.log('Render Counter Hooks');
     const [count, setCount] = useState(initialCount);
+    const style = useContext(ThemeContext);
 
     return (
         <div>
-            <button onClick={() => setCount(prevCount => prevCount - 1)}>
+            <button style={style} onClick={() => setCount(prevCount => prevCount - 1)}>
                 -
             </button>
             <span>{count}</span>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>
+            <button style={style} onClick={() => setCount(prevCount => prevCount + 1)}>
                 + 
             </button>
         </div>
